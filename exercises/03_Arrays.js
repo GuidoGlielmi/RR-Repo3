0b4c633ff724ea6078a7acc04c72fc8b6eb68a83
Guido Glielmi <guidoglielmi@gmail.com>
2021-10-07 14:53:32 -0300

    Exercise 3f resolved

--- exercises/03_Arrays.js
+++ exercises/03_Arrays.js
@@ -6,8 +6,8 @@
 // d) Quitar un elemento del principio y del final del array (utilizar shift y pop).
 // e) Invertir el orden del array (utilizar reverse).
 // f) Unir todos los elementos del array en un único string donde cada mes este separado
-// por un guión - (utilizar join).g) Crear una copia del array de meses que contenga desde Mayo hasta Noviembre
-// (utilizar slice).
+// por un guión - (utilizar join).g) Crear una copia del array de meses
+// que contenga desde Mayo hasta Noviembre (utilizar slice).
 
 var myArray = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 console.log(myArray[4]);
@@ -24,3 +24,9 @@ myArray.pop();
 console.log(myArray);
 
 console.log(myArray.reverse());
+
+var myArray = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+var myString = myArray.join(" - ");
+var myArrayCopy = myArray.slice(4,11);
+console.log(myString);
+console.log(myArrayCopy);
