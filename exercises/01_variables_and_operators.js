8f0923320a47dc35152a424896adfb2b4f921c13
Guido Glielmi <guidoglielmi@gmail.com>
2021-10-07 11:52:12 -0300

    Exercise 1c resolved

--- exercises/01_variables_and_operators.js
+++ exercises/01_variables_and_operators.js
@@ -1,5 +1,6 @@
 // Variables y Operadores
-// Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+// Crear dos variables numéricas y utilizar el operador suma
+// para guardar el valor de la suma de ambos números en una 3er variable.
 // Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 // Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
 // guardando el resultado de la suma en una 3er variable (utilizar length).
@@ -13,3 +14,8 @@ var string1 = 'string';
 var string2 = 'string';
 var concat = string1 + string2;
 console.log(concat);
+
+var string3 = 'hola';
+var string4 = 'chau';
+var sumConcat = string3.length + string4.length;
+console.log(sumConcat);
