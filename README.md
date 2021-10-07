e4a3934e4361e79ee2cabc160bea95a108da8a54
Guido Glielmi <guidoglielmi@gmail.com>
2021-10-07 11:48:18 -0300

    gitignore, index and readme added. Exercise 1a resolved

---
+++ README.md
@@ -0,0 +1,3 @@
+# Third repository
+Project derived from the fourth problematic of RR.  
+This is a series of JavaScript problems.
---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,10 @@
+// Variables y Operadores
+// Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+// Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+// Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+// guardando el resultado de la suma en una 3er variable (utilizar length).
+
+var variable1 = 4;
+var variable2 = 3;
+var sum = variable1 + variable2;
+console.log(sum);
